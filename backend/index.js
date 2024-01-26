7e1366078c975037acb04435a21b1a8a91ba020b
Juliano Santos <julianosantosdev@outlook.com>
2024-01-26 10:54:44 -0300

    deploy: index.js

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+import { createServer } from 'http'
+
+const PORT = process.env.PORT || 3000
+
+const server = createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end(`Hello ${process.env.HELLO}`)
+})
+
+server.listen(PORT, () => {
+  console.log(`Server running on port:${PORT}/`)
+})
